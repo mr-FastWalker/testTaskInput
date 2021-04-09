e9d20c314935bb682092022d07881a3030141b08
mr-FastWalker <mr.skorokhod@mail.ru>
2021-04-10 00:30:20 +1000

    test task - input

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <div>Пожалуйста, введите ваш текст</div>
+            <div>
+                <InputField/>
+            </div>
+        </div>
+    );
+}
+
+// InputField можно вынести в отдельный файл
+const InputField = () => {
+
+    let userText = ''
+
+    const [userInput, setUserInput] = useState('');
+
+    const handleChange = (e) => {
+        setUserInput(e.currentTarget.value)
+    }
+
+    const resetInput = (e) => {
+        setUserInput(e.currentTarget.value = '')
+    }
+
+    (userInput === '') ? userText = 'Здесь будет ваш текст' : userText = userInput;
+
+    return (
+        <div>
+            <input
+                className="myInputField"
+                value={userInput}
+                type="text"
+                onChange={handleChange}
+                placeholder="Введите значение..."
+            />
+            <button className="buttonClean"
+                    onClick={resetInput}
+            >Reset
+            </button>
+
+            <div className="outputText">
+                <h2>{userText}</h2>
+            </div>
+        </div>
+    )
 }
 
 export default App;
+
+
